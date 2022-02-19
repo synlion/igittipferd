592337313f9956bcef46c14d916341ee1690e7ee
synlion <leonard.reles@gmail.com>
2022-02-19 20:53:30 +0100

    @davidnettes joins the fray

--- tttrepo/tictactoe.py
+++ tttrepo/tictactoe.py
@@ -1,8 +1,8 @@
 # 18.02.2022
 # 21:31 
 
+# 19/02/22 davidnettes joins the effort to get the git fun going
+
 # Beginning of the tictactoe.py project. 
 
 print("Let's play TicTacToe!")
-
-print("Committing is fun!")
