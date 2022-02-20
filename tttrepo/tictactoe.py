a226780620188336d657fa22735905156a49c699
davidnettes <david.nettesheim@web.de>
2022-02-20 14:16:14 +0100

    test2

--- tttrepo/tictactoe.py
+++ tttrepo/tictactoe.py
@@ -3,12 +3,7 @@
 
 # 19/02/22 davidnettes joins the effort to get the git fun going
 
-# Beginning of the tictactoe.py project. 
-<<<<<<< HEAD
-# hi hi hi
-=======
-# hi hi
->>>>>>> 7a6e74d4872f29027308fb3e980c3e4b7c81d4ba
+# Beginning of the tictactoe.py project.
 import random
 
 print("Let's play TicTacToe!")
