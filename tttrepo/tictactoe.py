b97e5a785fb2180871663d862991b98cee2f575e
synlion <leonard.reles@gmail.com>
2022-02-25 19:22:37 +0100

    post-revert-merge clean up

--- tttrepo/tictactoe.py
+++ tttrepo/tictactoe.py
@@ -1,28 +1,15 @@
 # Project Inititalized: 18/02/2022 
 # 21:31 
 
-<<<<<<< Updated upstream
-# 19/02/22 davidnettes joins the effort to get the git fun going
-
-<<<<<<< HEAD
 # Beginning of the tictactoe.py project..
 
-=======
->>>>>>> Stashed changes
 import random
 
 print("Let's play TicTacToe!")
 
-# just printing it
+# a possible game board 
 print("""\n
 -|-|-
 -|-|-
 -|-|-
     \n""")
-=======
-# Beginning of the tictactoe.py project. 
-# hi hi
-import random
-
-print("Let's play TicTacToe!")
->>>>>>> parent of 917a611 (Merge pull request #2 from davidnettes/main)
